3d6806870150e63f7110717ef0ec55f77dfdf060
BasmaBenNasr <bennasrbasma97@gmail.com>
2021-05-23 01:10:27 +0100

    adding bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/Linked/header/header.component';
+import { FooterComponent } from './Components/Linked/footer/footer.component';
+import { NavComponent } from './Components/Linked/nav/nav.component';
+import { ShoppingCartComponent } from './Components/shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule
