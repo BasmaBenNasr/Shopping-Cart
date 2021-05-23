bcd052c9ad9bb1329057eef19c93b1e5fefbdc90
BasmaBenNasr <bennasrbasma97@gmail.com>
2021-05-23 14:37:05 +0100

    using bootstrap to create the UI desing of the shopping Cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { HeaderComponent } from './Components/Linked/header/header.component';
 import { FooterComponent } from './Components/Linked/footer/footer.component';
 import { NavComponent } from './Components/Linked/nav/nav.component';
 import { ShoppingCartComponent } from './Components/shopping-cart/shopping-cart.component';
+import { FiltersComponent } from './Components/filters/filters.component';
+import { ProductListComponent } from './Components/product-list/product-list.component';
+import { CartComponent } from './Components/cart/cart.component';
+import { CartItemComponent } from './Components/cart-item/cart-item.component';
+import { ProductItemComponent } from './Components/product-list/product-item/product-item.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +18,12 @@ import { ShoppingCartComponent } from './Components/shopping-cart/shopping-cart.
     HeaderComponent,
     FooterComponent,
     NavComponent,
-    ShoppingCartComponent
+    ShoppingCartComponent,
+    FiltersComponent,
+    ProductListComponent,
+    CartComponent,
+    CartItemComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule
